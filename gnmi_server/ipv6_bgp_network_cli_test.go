55303333b5271c7710664324dff946214d78a792
Zhaoyang Zhang <47635355+ZhaoyangZhang-MS@users.noreply.github.com>
2025-09-23 14:27:56 +0800

    Enrich unit testing for 'show ipv6 bgp network' (#121)

--- gnmi_server/ipv6_bgp_network_cli_test.go
+++ gnmi_server/ipv6_bgp_network_cli_test.go
@@ -6,6 +6,7 @@ package gnmi
 
 import (
 	"crypto/tls"
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -68,11 +69,16 @@ func TestGetIPv6BGPNetwork(t *testing.T) {
 				elem: <name: "bgp" >
 				elem: <name: "network" >
 			`
-		wantRespVal := `
-		{
-			"output": "BGP table version is 6405, local router ID is 10.1.0.32, vrf id 0\nDefault local pref 100, local AS 64601\nStatus codes:  s suppressed, d damped, h history, * valid, > best, = multipath,\n               i internal, r RIB-failure, S Stale, R Removed\nNexthop codes: @NNN nexthop's vrf id, < announce-nh-self\nOrigin codes:  i - IGP, e - EGP, ? - incomplete\nRPKI validation codes: V valid, I invalid, N Not found\n\n    Network          Next Hop            Metric LocPrf Weight Path\n *= ::/0             fc00::12                               0 64802 65534 6666 6667 i\n *>                  fc00::2                                0 64802 65534 6666 6667 i\n *=                  fc00::1a                               0 64802 65534 6666 6667 i\n *=                  fc00::a                                0 64802 65534 6666 6667 i\n *> 2064:100::1/128  fc00::2                                0 64802 i\n *> 2064:100::2/128  fc00::a                                0 64802 i\n *> 2064:100::3/128  fc00::12                               0 64802 i\n *> 2064:100::4/128  fc00::1a                               0 64802 i\n *= 20c0:a808::/64   fc00::12                               0 64802 64602 i\n *>                  fc00::2                                0 64802 64602 i\n *=                  fc00::1a                               0 64802 64602 i\n *=                  fc00::a                                0 64802 64602 i\n *= 20c0:a808:0:80::/64\n                    fc00::12                               0 64802 64602 i\n *>                  fc00::2                                0 64802 64602 i\n *=                  fc00::1a                               0 64802 64602 i\n *=                  fc00::a                                0 64802 64602 i\n *= 20c0:a810::/64   fc00::12                               0 64802 64603 i\n *>                  fc00::2                                0 64802 64603 i\n *=                  fc00::1a                               0 64802 64603 i\n *=                  fc00::a                                0 64802 64603 i\n\nDisplayed 8 routes and 20 total paths"
+		content, err := os.ReadFile(showIpv6BgpNetworkMockFile)
+		if err != nil {
+			t.Fatalf("failed to read file: %v", err)
 		}
-		`
+		wantRespVal := fmt.Sprintf(`
+		{
+			"output": %q
+		}
+		`, content)
+
 		runTestGet(t, ctx, gClient, "SHOW", textPbPath, codes.OK, wantRespVal, true)
 	})
 	patches.Reset()
@@ -86,17 +92,21 @@ func TestGetIPv6BGPNetwork(t *testing.T) {
 				elem: <name: "network" >
 				elem: <name: "2064:100::2/128" >
 			`
-		wantRespVal := `
-		{
-			"output": "BGP routing table entry for 2064:100::2/128, version 5\nPaths: (1 available, best #1, table default)\n  Advertised to non peer-group peers:\n  fc00::2 fc00::a fc00::12 fc00::1a\n  64802\n    fc00::a from fc00::a (100.1.0.2)\n      Origin IGP, valid, external, best (First path received)\n      Last update: Wed Aug 27 22:46:48 2025"
+		content, err := os.ReadFile(showIpv6BgpNetworkAddressMockFile)
+		if err != nil {
+			t.Fatalf("failed to read file: %v", err)
 		}
-		`
+		wantRespVal := fmt.Sprintf(`
+		{
+			"output": %q
+		}
+		`, content)
 		runTestGet(t, ctx, gClient, "SHOW", textPbPath, codes.OK, wantRespVal, true)
 	})
 	patches.Reset()
 
-	showIpv6BgpNetworkAddressIBestpathMockFile := "../testdata/show_ipv6_bgp_network_address_json.txt"
-	patches = MockNSEnterOutput(t, showIpv6BgpNetworkAddressIBestpathMockFile)
+	showIpv6BgpNetworkAddressIJsonMockFile := "../testdata/show_ipv6_bgp_network_address_json.txt"
+	patches = MockNSEnterOutput(t, showIpv6BgpNetworkAddressIJsonMockFile)
 	t.Run("SHOW ipv6 bgp network 2064:100::2/128 json", func(t *testing.T) {
 		textPbPath := `
 				elem: <name: "ipv6" >
@@ -105,10 +115,33 @@ func TestGetIPv6BGPNetwork(t *testing.T) {
 				elem: <name: "2064:100::2/128" >
 				elem: <name: "json" >
 			`
-		wantRespVal, err := os.ReadFile("../testdata/show_ipv6_bgp_network_address_json.txt")
+		wantRespVal, err := os.ReadFile(showIpv6BgpNetworkAddressIJsonMockFile)
 		if err != nil {
 			t.Fatalf("failed to read file: %v", err)
 		}
+		runTestGet(t, ctx, gClient, "SHOW", textPbPath, codes.OK, wantRespVal, true)
+	})
+	patches.Reset()
+
+	showIpv6BgpNetworkAddressIBestpathMockFile := "../testdata/show_ipv6_bgp_network_address_bestpath.txt"
+	patches = MockNSEnterOutput(t, showIpv6BgpNetworkAddressIBestpathMockFile)
+	t.Run("SHOW ipv6 bgp network 20c0:a800:0:592::/64 bestpath", func(t *testing.T) {
+		textPbPath := `
+				elem: <name: "ipv6" >
+				elem: <name: "bgp" >
+				elem: <name: "network" >
+				elem: <name: "20c0:a800:0:592::/64" >
+				elem: <name: "bestpath" >
+			`
+		content, err := os.ReadFile(showIpv6BgpNetworkAddressIBestpathMockFile)
+		if err != nil {
+			t.Fatalf("failed to read file: %v", err)
+		}
+		wantRespVal := fmt.Sprintf(`
+		{
+			"output": %q
+		}
+		`, content)
 		runTestGet(t, ctx, gClient, "SHOW", textPbPath, codes.OK, wantRespVal, true)
 	})
 	patches.Reset()
